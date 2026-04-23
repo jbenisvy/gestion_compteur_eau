97ca98f1358360f7c23e4e0681e0e1824435e1ab
Benisvy Johny <johny.benisvy@gmail.com>
2026-04-23 23:14:57 +0200

    cumul role copro et syndic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,6 +16,10 @@ class HomeController extends AbstractController
                 return $this->redirectToRoute('admin_dashboard');
             }
 
+            if ($this->isGranted('ROLE_USER') && $this->isGranted('ROLE_SYNDIC')) {
+                return $this->redirectToRoute('role_switch');
+            }
+
             if ($this->isGranted('ROLE_SYNDIC')) {
                 return $this->redirectToRoute('syndic_dashboard');
             }
@@ -24,5 +28,27 @@ class HomeController extends AbstractController
         }
 
         return $this->render('home/landing.html.twig');
+    }
+
+    #[Route('/choix-espace', name: 'role_switch')]
+    public function roleSwitch(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        if (!($this->isGranted('ROLE_USER') && $this->isGranted('ROLE_SYNDIC'))) {
+            if ($this->isGranted('ROLE_SYNDIC')) {
+                return $this->redirectToRoute('syndic_dashboard');
+            }
+
+            return $this->redirectToRoute('copro_dashboard');
+        }
+
+        return $this->render('home/role_switch.html.twig');
     }
 }
